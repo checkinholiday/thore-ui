c53d02aa20befa1d68d3d2a28bcff531c5a787e0
Marc <jackob.dev@gmail.com>
2023-06-24 23:52:29 +0200

    feat: Button Darkmode

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,51 +4,52 @@ export interface ButtonProps {
   children: any;
   type: 'primary' | 'secondary' | 'success' | 'warning' | 'error';
   disabled?: boolean;
+  dark?: boolean;
 }
 
-export function Button({ children, type, disabled = false }: ButtonProps) {
+export function Button({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
 }
 
-export function ElevatedButton({ children, type, disabled = false }: ButtonProps) {
+export function ElevatedButton({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`elevated-button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`elevated-button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
 }
 
-export function OutlinedButton({ children, type, disabled = false }: ButtonProps) {
+export function OutlinedButton({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`outlined-button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`outlined-button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
 }
 
-export function RoundedButton({ children, type, disabled = false }: ButtonProps) {
+export function RoundedButton({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`rounded-button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`rounded-button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
 }
 
-export function FlatButton({ children, type, disabled = false }: ButtonProps) {
+export function FlatButton({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`flat-button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`flat-button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
 }
 
-export function LightButton({ children, type, disabled = false }: ButtonProps) {
+export function LightButton({ children, type, disabled = false, dark = false }: ButtonProps) {
   return (
-    <button className={`light-button ${type} ${disabled ? 'disabled' : ''}`}>
+    <button className={`light-button ${type} ${disabled ? 'disabled' : ''} ${dark ? 'dark' : ''}`}>
       {children}
     </button>
   );
